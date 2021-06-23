21fdfbd6aaef508ca2814e932696d2c5b54fab09
jcolon-1993 <60831920+jcolon-1993@users.noreply.github.com>
2021-06-22 17:36:19 -0700

    Update Comments

--- js/google-maps.js
+++ js/google-maps.js
@@ -1,3 +1,5 @@
+/* loads the map into the HTML page. First it
+creates a mapOptions object with three properties. Uses object literal notation */
 function init()
 {
   // Set up map options
@@ -7,11 +9,15 @@ function init()
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     zoom: 13
   };
+
+  /* uses the Map () constructor to create a map and draw the map into the page */
   // Map() draws a map
   var venueMap;
   venueMap = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
 
+/* creates a <script> element to load the Google Maps API. When it has loaded,
+  it calls init(), to initialize the map.  */
 function loadScript()
 {
   // Create <Script> element
@@ -20,4 +26,6 @@ function loadScript()
   document.body.appendChild(script);
 }
 // Onload call
+/* when the page has loaded, the on load event will call the loadScript
+  () function. */
 window.onload = loadScript;
